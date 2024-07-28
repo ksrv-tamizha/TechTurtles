24c2c3d93a1cda2d04324606a9a2668a3d75ccb4
ksrv-tamizha <160010200+ksrv-tamizha@users.noreply.github.com>
2024-07-28 11:40:24 +0530

    Add files via upload

---
+++ 09.py
@@ -0,0 +1,3 @@
+s1={0, 1, 4, 5, 6, 7, 8, 9, 11, 45}
+s2={0, 34, 55, 23, 89, 111}
+print("No Common" if s1.isdisjoint(s2) else "Has Common")
---
+++ 10.py
@@ -0,0 +1,4 @@
+se={0, 1, 4, 5, 6, 7, 8, 9, 11, 45}
+print(se)
+x=sorted(list(se),reverse=True)
+print(x[2])
